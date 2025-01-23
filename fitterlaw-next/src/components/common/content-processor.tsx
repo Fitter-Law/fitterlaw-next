3f59eb5808464971156725de812f736e77baf77b
F898592 <client.matters@fitterlaw.com>
2025-01-23 02:35:39 -0600

    processor fixed

--- fitterlaw-next/src/components/common/content-processor.tsx
+++ fitterlaw-next/src/components/common/content-processor.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import DOMPurify from 'isomorphic-dompurify';
 
-const ContentProcessor = ({ content }) => {
+interface ContentProcessorProps {
+  content: string;
+}
+
+const ContentProcessor = ({ content }: ContentProcessorProps) => {
   // Clean and sanitize WordPress content
   const cleanContent = DOMPurify.sanitize(content, {
     ADD_TAGS: ['iframe'],
